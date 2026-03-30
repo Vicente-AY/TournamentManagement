1e9a54cba1eb12e8afd3f245cd35132d84da9c02
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-03-30 12:13:19 +0200

    Creado metodo para añadir Jugadores a los equipos

--- src/team/Player.java
+++ src/team/Player.java
@@ -20,6 +20,31 @@ public class Player {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Type the id of the player");
+        int id = input.nextInt();
+        input.nextLine();
 
+        Team teamToAddPlayer = null;
+
+        for(Team team : teams) {
+            if(team.id == id){
+                teamToAddPlayer = team;
+            }
+        }
+
+        if(teamToAddPlayer != null){
+            System.out.println("Introduce the name of the Player");
+            String name = input.nextLine();
+
+            System.out.println("Introduce the surname of the Player");
+            String surname = input.nextLine();
+
+            Player player = new Player(id, name, surname);
+            teamToAddPlayer.members.add(player);
+            System.out.println("Player " + name + " " + surname + " with ID: " + id + " has been added to the team");
+        }
+        else{
+            System.out.println("ID not found");
+        }
     }
 }
